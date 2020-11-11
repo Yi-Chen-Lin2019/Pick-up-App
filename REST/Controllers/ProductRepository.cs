8f0a7deb5690138fd98a5b015be4ac64c94db571
Yi-Chen Lin <1081501@ucn.dk>
2020-11-11 13:12:53 +0100

    add api with authentication and swagger

---
+++ REST/Controllers/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using REST.Models;
+using System.Collections.Generic;
+
+namespace REST.Controllers
+{
+    internal class ProductRepository : IProductRepository
+    {
+        public Product Get(int productID)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Product GetProductById(int productID)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
