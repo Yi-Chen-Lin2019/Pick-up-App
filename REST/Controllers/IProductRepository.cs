8064fd2b69b2f1b841736197b8d144bd7f3e547f
Yi-Chen Lin <1081501@ucn.dk>
2020-11-11 13:12:53 +0100

    add api with authentication and swagger

---
+++ REST/Controllers/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using REST.Models;
+using System.Collections.Generic;
+
+namespace REST.Controllers
+{
+    internal interface IProductRepository
+    {
+        void InsertProduct(Product product);
+        Product Get(int productID);
+        Product GetProductById(int productID);
+        List<Product> GetAllProducts();
+    }
+}
