9e2525a605feb5205a4f8e08a861cd53e279b7c2
mikaske <mikaskentra@gmail.com>
2020-11-13 00:40:29 +0100

    nothing serious done, just a try-out

---
+++ WebUI/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebUI.Models;
+
+namespace WebUI.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Category> categories { get; set; }
+        public IEnumerable<Product> products { get; set; }
+    }
+}
