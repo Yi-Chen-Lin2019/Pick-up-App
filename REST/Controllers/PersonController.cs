4483fed1eb2b388364a5a19336d8630a5803ed8d
Yi-Chen Lin <1081501@ucn.dk>
2020-11-15 16:43:50 +0100

    use AspNetUser via dapper role patten is not figured out yet

---
+++ REST/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace REST.Controllers
+{
+    public class PersonController : ApiController
+    {
+        private readonly IDbConnection _db;
+        public PersonController()
+        {
+            _db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("Users/GetAllUsers")]
+        public List<Person> GetAllUsers()
+        {
+            List<Person> output = this._db.Query<Person>
+             ("SELECT TOP (1000) [AspNetUsers].[Id],[Email] FROM [PikUpAppAccountData].[dbo].[AspNetUsers]").ToList();
+            return output;
+
+        
+        }
+    }
+}
