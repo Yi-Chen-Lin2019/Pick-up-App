ec418e562e794bfc4e6f3ac786bbf74d2ae300a4
Yi-Chen Lin <1081501@ucn.dk>
2020-11-16 16:10:43 +0100

    remove models.product. Instead, use model project

--- REST/Models/Order.cs
+++ REST/Models/Order.cs
@@ -15,6 +15,7 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
+using Model;
 
 namespace REST.Models
 {
