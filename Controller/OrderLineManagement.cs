dfed950949ec07c1728e05c40dd3adfcddf76ae2
Yi-Chen Lin <1081501@ucn.dk>
2020-11-16 21:59:22 +0100

    move stub to DAL layer

---
+++ Controller/OrderLineManagement.cs
@@ -0,0 +1,42 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class OrderLineManagement
+    {
+        public IEnumerable<OrderLine> GetAll()
+        {
+            IOrderLineRepository pRepo = new OrderLineRepository();
+            return pRepo.GetAllOrderLines();
+
+        }
+
+        public OrderLine DeleteOrderLine(int orderLineID)
+        {
+            IOrderLineRepository pRepo = new OrderLineRepository();
+            return pRepo.DeleteOrderLine(orderLineID);
+        }
+
+        public OrderLine UpdateOrderLine(OrderLine orderLine)
+        {
+            IOrderLineRepository pRepo = new OrderLineRepository();
+            return pRepo.UpdateOrderLine(orderLine);
+        }
+
+        public OrderLine InsertOrderLine(OrderLine orderLine)
+        {
+            IOrderLineRepository pRepo = new OrderLineRepository();
+            return pRepo.InsertOrderLine(orderLine);
+        }
+
+        public OrderLine GetOrderLineById(int orderLineID)
+        {
+            IOrderLineRepository pRepo = new OrderLineRepository();
+            return pRepo.GetOrderLineById(orderLineID);
+        }
+    }
+}
