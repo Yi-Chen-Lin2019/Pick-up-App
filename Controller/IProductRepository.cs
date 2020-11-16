bac2e204292504dad6779e5cb70292a3c821f2ca
Yi-Chen Lin <1081501@ucn.dk>
2020-11-16 15:28:52 +0100

    change the name of  Controller to BusinessLayer + add productManagement  and orderManagement class

---
+++ Controller/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    internal interface IProductRepository
+    {
+        Product InsertProduct(Product product);
+        Product Get(int productID);
+        Product GetProductById(int productID);
+        List<Product> GetAllProducts();
+        Product UpdateProduct(int id, Product product);
+        Product DeleteProduct(int id);
+        Product UpdateProduct(Product product);
+    }
+}
---
+++ Controller/ProductManagement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class ProductManagement
+    {
+
+        public IEnumerable<Product> GetAll()
+        {
+            IProductRepository pRepo = new ProductRepository();
+            return  pRepo.GetAllProducts();
+            
+        }
+        
+ 
+    }
+}
