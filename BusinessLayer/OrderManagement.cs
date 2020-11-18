66715c516c6b5f691a171235e28af3a1569903cf
Yi-Chen Lin <1081501@ucn.dk>
2020-11-18 14:01:41 +0100

    this project is referenced in api, don't call them anywhere else

---
+++ BusinessLayer/OrderManagement.cs
@@ -0,0 +1,42 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class OrderManagement
+    {
+        public IEnumerable<Order> GetAllOrders()
+        {
+            IOrderRepository pRepo = new OrderRepository();
+            return pRepo.GetAllOrders();
+
+        }
+
+        public Order DeleteOrder(int orderID)
+        {
+            IOrderRepository pRepo = new OrderRepository();
+            return pRepo.DeleteOrder(orderID);
+        }
+
+        public Order UpdateOrder(Order order)
+        {
+            IOrderRepository pRepo = new OrderRepository();
+            return pRepo.UpdateOrder(order);
+        }
+
+        public Order InsertOrder(Order order)
+        {
+            IOrderRepository pRepo = new OrderRepository();
+            return pRepo.InsertOrder(order);
+        }
+
+        public Order GetOrderById(int orderID)
+        {
+            IOrderRepository pRepo = new OrderRepository();
+            return pRepo.GetOrderById(orderID);
+        }
+    }
+}
