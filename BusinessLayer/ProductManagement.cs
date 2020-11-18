9a9a4aacb5511ee804353b6e0a2c8bc8605f0239
Yi-Chen Lin <1081501@ucn.dk>
2020-11-18 14:01:41 +0100

    this project is referenced in api, don't call them anywhere else

---
+++ BusinessLayer/ProductManagement.cs
@@ -0,0 +1,43 @@
+﻿using Model;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class ProductManagement
+    {
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            IProductRepository pRepo = new ProductRepository();
+            return  pRepo.GetAllProducts();
+            
+        }
+
+        public Product DeleteProduct(int productID)
+        {
+            IProductRepository pRepo = new ProductRepository();
+            return pRepo.DeleteProduct(productID);
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            IProductRepository pRepo = new ProductRepository();
+            return pRepo.UpdateProduct(product);
+        }
+
+        public Product InsertProduct(Product product)
+        {
+            IProductRepository pRepo = new ProductRepository();
+            return pRepo.InsertProduct(product);
+        }
+
+        public Product GetProductById(int productID)
+        {
+            IProductRepository pRepo = new ProductRepository();
+            return pRepo.GetProductById(productID);
+        }
+    }
+}
