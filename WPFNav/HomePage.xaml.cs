db02b4e43897814785cf411831dba5553b4f9814
minezspeletajs <minezspeletajs@gmail.com>
2020-11-29 15:39:59 +0100

    wpf branch upload with create product button

--- WPFNav/HomePage.xaml.cs
+++ WPFNav/HomePage.xaml.cs
@@ -24,5 +24,11 @@ namespace WPFNav
         {
             InitializeComponent();
         }
+
+        //private void CreateProductButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    CreateProductFrame.Content = new CreateProductPage();
+        //}
+
     }
 }
