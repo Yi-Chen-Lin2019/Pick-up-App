6d9d45291f562f722d4c5928879a513771fd58c9
RodioserPL <rodioserpl@gmail.com>
2020-12-02 13:16:26 +0100

    UI changes

--- WebUI/ViewModels/BrowseViewModel.cs
+++ WebUI/ViewModels/BrowseViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,6 @@ namespace WebUI.ViewModels
     public class BrowseViewModel
     {
         public IEnumerable<CategoryViewModel> categories { get; set; }
-        public IEnumerable<ProductViewModel> products { get; set; }
+        public IPagedList<ProductViewModel> products { get; set; }
     }
 }
