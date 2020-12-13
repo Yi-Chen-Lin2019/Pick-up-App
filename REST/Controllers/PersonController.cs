1a876497a77348dcb6b666189a5c437b4dde97b0
Yi-Chen Lin <1081501@ucn.dk>
2020-12-13 12:25:27 +0100

    Person/UserId in REST

--- REST/Controllers/PersonController.cs
+++ REST/Controllers/PersonController.cs
@@ -60,22 +60,20 @@ namespace REST.Controllers
         /// By passing in the username, you can get the person of the username in the system. 
         /// </summary>
         /// <exception cref="IO.Swagger.Client.ApiException">Thrown when fails to make API call</exception>
-        /// <param name="userName">Put in person username.</param>
         /// <returns>List&lt;Person&gt;</returns>
         /// <response code = "200">Person found</response>
         /// <response code = "404">Person not found</response>
-        [Route("Person/UserName")]
+        [Route("Person/UserId")]
         [HttpGet]
         [Authorize(Roles = "Customer")]
         [ResponseType(typeof(Person))]
-        public IHttpActionResult Get(String userName)
+        public IHttpActionResult GetMyInfo()
         {
-            if (RequestContext.Principal.Identity.GetUserName().Equals(userName))
-            {
+            
                 try
                 {
                     PersonManagement pm = new PersonManagement();
-                    Person result = pm.GetPersonByUserName(userName);
+                    Person result = pm.GetPersonByUserName(RequestContext.Principal.Identity.GetUserId());
                     return Ok(result);
                 }
                 catch (Exception e)
@@ -83,10 +81,7 @@ namespace REST.Controllers
                     Console.WriteLine(e);
                     return InternalServerError();
                 }
-            } else
-            {
-                return BadRequest();
-            }
+           
                 
         }
 
